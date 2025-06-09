e61cba3b90c95de0740d7c200ab2600ff727a061
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-06-09 11:08:39 +0800

    Deploy: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 import { CustomToaster } from "@/components/custom-toaster";
@@ -72,6 +73,7 @@ export default function RootLayout({
           </HydrateClient>
         </TRPCReactProvider>
       </body>
+      <GoogleAnalytics gaId="G-J3GN6BMKJC" />
     </html>
   );
 }

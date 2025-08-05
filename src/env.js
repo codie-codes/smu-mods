9122f956959a706f689137ad16024de8070c3008
Isaiah Chia <130481550+Isaidchia@users.noreply.github.com>
2025-08-05 16:48:33 +0800

    added posthog-js
    - pnpm add
    - updated env.js

--- src/env.js
+++ src/env.js
@@ -20,6 +20,8 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: z.string().default("development"),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string().default("development"),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string().default("development"),
   },
 
   /**
@@ -31,6 +33,8 @@ export const env = createEnv({
     NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA:
       process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
